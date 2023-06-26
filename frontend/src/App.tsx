1e504f5b51fd7a5001ae916e552908ebfd77276b
Malkoc Mirza <81041253+Mirza-bot@users.noreply.github.com>
2023-06-26 19:29:56 +0200

    leftovers from redux terminated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,13 @@
-import { Provider } from "react-redux";
-import store from "./store/store";
 import Login from "./pages/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/login" Component={Login}></Route>
-        </Routes>
-      </Router>
-    </Provider>
+    <Router>
+      <Routes>
+        <Route path="/login" Component={Login}></Route>
+      </Routes>
+    </Router>
   );
 }
 
