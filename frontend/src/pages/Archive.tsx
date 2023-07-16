396e6c37db974fa97a9d67dbc8c4c2a3cd8c97b9
Malkoc Mirza <81041253+Mirza-bot@users.noreply.github.com>
2023-07-16 08:05:39 +0200

    slight improvements to archive Ui

--- frontend/src/pages/Archive.tsx
+++ frontend/src/pages/Archive.tsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react";
 import { useApplicationStore } from "../../features/store/applications";
 import ArchiveItem from "../components/layout/ArchiveItem";
+import { ApplicationArray } from "../../../shared/types";
+import { BiArchiveIn } from "react-icons/bi";
 
 function Archive() {
-  const applications = useApplicationStore((state) => state.applications);
+  const { applications, getAllApplications } = useApplicationStore(
+    (state) => state
+  );
 
-  return (
-    <div className=" flex flex-col gap-1">
-      {applications.map((application) => {
-        if (application.status === "Archived") {
+  const [archiveApplications, setArchiveApplications] =
+    useState<ApplicationArray>([]);
+
+  useEffect(() => {
+    if (applications.length === 0) {
+      getAllApplications();
+    }
+    const archiveItems = applications.filter(
+      (application) => application.status === "Archived"
+    );
+    setArchiveApplications(archiveItems);
+    console.log(archiveItems);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [applications]);
+
+  if (archiveApplications.length === 0) {
+    return (
+      <div className="text-grey text-2xl text-center w-full flex flex-col absolute top-1/3">
+        <span>No items in the archive yet</span>
+        <span className="text-5xl mx-auto mt-5">
+          <BiArchiveIn />
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className=" flex flex-col gap-1">
+        {archiveApplications.map((application) => {
           return (
             <ArchiveItem
               key={application._id}
@@ -22,9 +51,9 @@ function Archive() {
               notes={application.notes}
             />
           );
-        }
-      })}
-    </div>
-  );
+        })}
+      </div>
+    );
+  }
 }
 export default Archive;
